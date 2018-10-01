665b6f9d1c191afa252b4dde9bc67ddca39257b8
praveensinghkmr <praveensingh.kmr@gmail.com>
2018-10-01 14:57:03 +0530

    Removed Magic numbers

--- SpaceAge.cs
+++ SpaceAge.cs
@@ -1,13 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 public class SpaceAge
 {
     double seconds;
-    long earthYear;
+    const long earthYear = 31557600;
+    static Dictionary<string, double> earthYearFactor = new Dictionary<string, double>
+    {
+        {"Mercury", 0.2408467},
+        {"Venus", 0.61519726 },
+        {"Mars", 1.8808158 },
+        {"Jupiter", 11.862615 },
+        {"Saturn", 29.447498 },
+        {"Uranus", 84.016846 },
+        {"Neptune", 164.79132 }
+    };
+
     public SpaceAge(long seconds)
     {
         this.seconds = seconds;
-        earthYear = 31557600;
     }
 
     public double OnEarth()
@@ -17,34 +28,34 @@ public class SpaceAge
 
     public double OnMercury()
     {
-        return Math.Round(seconds / (earthYear * 0.2408467), 2); 
+        return Math.Round(seconds / (earthYear * earthYearFactor["Mercury"]), 2); 
     }
 
     public double OnVenus()
     {
-        return Math.Round(seconds / (earthYear * 0.61519726), 2);
+        return Math.Round(seconds / (earthYear * earthYearFactor["Venus"]), 2);
     }
 
     public double OnMars()
     {
-        return Math.Round(seconds / (earthYear * 1.8808158), 2);
+        return Math.Round(seconds / (earthYear * earthYearFactor["Mars"]), 2);
     }        
     public double OnJupiter()
     {
-        return Math.Round(seconds / (earthYear * 11.862615), 2);
+        return Math.Round(seconds / (earthYear * earthYearFactor["Jupiter"]), 2);
     }
     public double OnSaturn()
     {
-        return Math.Round(seconds / (earthYear * 29.447498), 2);
+        return Math.Round(seconds / (earthYear * earthYearFactor["Saturn"]), 2);
     }
 
     public double OnUranus()
     {
-        return Math.Round(seconds / (earthYear * 84.016846), 2);
+        return Math.Round(seconds / (earthYear * earthYearFactor["Uranus"]), 2);
     }
 
     public double OnNeptune()
     {
-        return Math.Round(seconds / (earthYear * 164.79132), 2);
+        return Math.Round(seconds / (earthYear * earthYearFactor["Neptune"]), 2);
     }
 }
